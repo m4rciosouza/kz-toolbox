7a4dfd87e297c75f5069051a7ee72fde80ddd054
Marcio <m4rcio.souza@gmail.com>
2019-09-27 13:24:17 +0100

    Remover método main da classe de árvore.

--- src/main/java/com/kazale/kztoolbox/algoritmos/arvores/Arvore.java
+++ src/main/java/com/kazale/kztoolbox/algoritmos/arvores/Arvore.java
@@ -19,23 +19,4 @@ public class Arvore<T> {
         return raiz.toString();
     }
 
-    public static void main(String[] args) {
-        Arvore<Integer> arvore = new Arvore<>(1);
-        arvore.getRaiz().getFilhos().add(new Node<Integer>(11));
-        arvore.getRaiz().getFilhos().add(new Node<Integer>(12));
-        arvore.getRaiz().getFilhos().add(new Node<Integer>(13));
-
-        arvore.getRaiz().getFilhos().get(0).getFilhos().add(new Node<Integer>(21));
-        arvore.getRaiz().getFilhos().get(0).getFilhos().add(new Node<Integer>(22));
-        arvore.getRaiz().getFilhos().get(0).getFilhos().add(new Node<Integer>(23));
-
-        arvore.getRaiz().getFilhos().get(2).getFilhos().add(new Node<Integer>(23));
-        arvore.getRaiz().getFilhos().get(2).getFilhos().get(0).getFilhos().add(new Node<Integer>(33));
-        arvore.getRaiz().getFilhos().get(2).getFilhos().get(0).getFilhos().add(new Node<Integer>(34));
-        arvore.getRaiz().getFilhos().get(2).getFilhos().get(0).getFilhos().get(1).getFilhos().add(new Node<Integer>(43));
-
-        System.out.println(arvore);
-        System.out.println(Utils.formatarArvore(arvore));
-    }
-
 }
