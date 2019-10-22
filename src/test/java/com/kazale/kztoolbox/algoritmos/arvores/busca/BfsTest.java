c1e655f5d50051f69921db203155f18db01deb53
Marcio <m4rcio.souza@gmail.com>
2019-10-22 14:24:03 +0100

    Criada classe de teste para o algoritmo de BFS.

---
+++ src/test/java/com/kazale/kztoolbox/algoritmos/arvores/busca/BfsTest.java
@@ -0,0 +1,65 @@
+package com.kazale.kztoolbox.algoritmos.arvores.busca;
+
+import com.kazale.kztoolbox.algoritmos.arvores.Arvore;
+import com.kazale.kztoolbox.algoritmos.arvores.Node;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class BfsTest {
+
+    @Test
+    public void testBfsModoIterativo() {
+        Arvore<String> arvore = obterArvoreExemplo();
+        List<Node<String>> visitados = new ArrayList<>();
+
+        Bfs.iterativo(arvore.getRaiz(), visitados);
+
+        String caminhoPercorrido = visitados.stream()
+                .map(Node::getValor)
+                .collect(Collectors.joining());
+
+        assertEquals("ABCDFGHIJK", caminhoPercorrido);
+    }
+
+    /**
+     * Estrutura da árvore
+     *
+     * A
+     * |--B
+     * |  |--F
+     * |  |--G
+     * |--C
+     * |  |--H
+     * |--D
+     * |  |--I
+     * |  |--J
+     * |  |--K
+     */
+    private Arvore<String> obterArvoreExemplo() {
+        Arvore<String> arvore = new Arvore<>("A");
+
+        Node<String> node1 = new Node<>("B", arvore.getRaiz());
+        arvore.getRaiz().getFilhos().add(node1);
+        Node<String> node2 = new Node<>("C", arvore.getRaiz());
+        arvore.getRaiz().getFilhos().add(node2);
+        Node<String> node3 = new Node<>("D", arvore.getRaiz());
+        arvore.getRaiz().getFilhos().add(node3);
+
+        node1.getFilhos().add(new Node<>("F", node1));
+        node1.getFilhos().add(new Node<>("G", node1));
+
+        node2.getFilhos().add(new Node<>("H", node2));
+
+        node3.getFilhos().add(new Node<>("I", node3));
+        node3.getFilhos().add(new Node<>("J", node3));
+        node3.getFilhos().add(new Node<>("K", node3));
+
+        return arvore;
+    }
+
+}
